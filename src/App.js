fd4cca60d232f3f1b5047b81ed0c5336014adc33
kyrylo <kirillomelcenko411@gmail.com>
2020-11-03 00:48:18 +0200

    react

---
+++ src/App.js
@@ -0,0 +1,164 @@
+import './style/style.css';
+
+
+const App = () =>
+ 
+// const block__element = ({title, url='#'}) =>
+ 
+	
+
+	<div className="headerAll">
+		<div class="container">
+                  <div class="project__name"> 
+                      <img class="factor" src="http://localhost:3000/Fill 4.png" alt="factor"/>
+                  </div>
+                     <nav class="nav">
+                         <ul class="nav__ul"> 
+                            <li><a class="nav__a" href=""> Services </a></li>           
+                            <li><a class="nav__a" href=""> Portfolio </a></li>           
+                            <li><a class="nav__a" href=""> About </a></li>
+                            <li><a class="nav__a" href=""> Contact </a></li>
+                            <li> | </li>
+                            <li><a class="nav__a" href=""> Discus Your Idea </a></li>
+                        </ul>
+                     </nav>
+
+                  <div class="menu__email">        
+                      <div class="email"> 
+                      <a href=""> hello@digitfactor.com </a> 
+                      </div>        
+                      <div class="menu"> <a href=""> Menu </a> </div> 
+                   </div>  
+                    <div class="gamburger">
+                      <button> <svg  width="50" height="12" viewBox="0 0 50 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M50 0H0V1H50V0ZM50 11H0V12H50V11Z" fill="white"/></svg></button>
+                    </div>    
+        </div>             
+    
+        <div class="heading">
+                <div class="heading__inner">
+                    <div class="welcome">
+                        <svg width="20" height="16" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M12 0H8L0 16H4L12 0ZM20 0H16L8 16H12L20 0Z" fill="#FFCA00"/>
+                            </svg> <span>Wlecome to Digit Factor</span>                         
+                    </div>
+                    <div class="h3">
+                           <h3> Creative Web prodaction from Ukraine 
+                           </h3>                            
+                    </div>
+                        <button class="square">
+                            <div class="discus">Discus your idea</div> 
+                            <div class="circle"></div>
+                        </button>
+                </div>
+                <a href="#bottom"><div class="arrow">
+                      
+                    <svg width="8" height="48" viewBox="0 0 8 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M7.87969 44.2206C8.0401 44.063 8.0401 43.8006 7.87969 43.6375C7.72469 43.4798 7.46671 43.4798 7.31208 43.6375L4.40466 46.5945L4.40466 0.408252C4.4043 0.180792 4.22654 0 4.00289 0C3.77924 0 3.5957 0.180792 3.5957 0.408252L3.5957 46.5945L0.693704 43.6375C0.533285 43.4798 0.274952 43.4798 0.120314 43.6375C-0.0401049 43.8006 -0.0401049 44.0634 0.120314 44.2206L3.71601 47.8776C3.87101 48.0408 4.12899 48.0408 4.28362 47.8776L7.87969 44.2206Z" fill="#7E7E7E"/>
+                        </svg>
+                    Scroll down
+                </div></a>
+                <div class="chat">
+                    <a href=""><svg width="34" height="26" viewBox="0 0 34 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M29.3377 4.80738V11.9809C29.3377 14.636 27.1488 16.7883 24.4485 16.7883H12.015L6.38496 22.2345C6.18437 22.4285 5.91812 22.5314 5.64736 22.5314C5.51045 22.5314 5.37255 22.5052 5.24122 22.451C4.85052 22.29 4.5962 21.914 4.5962 21.4975V16.7789C2.03254 16.6299 0 14.5393 0 11.9809V4.80738C0 2.15234 2.18894 0 4.88912 0H24.4486C27.1487 0 29.3377 2.15234 29.3377 4.80738ZM31.4399 12.7387V12.3689C32.8857 12.5627 34.0001 13.7809 34 15.2555V19.6041C34 21.155 32.7679 22.4224 31.2137 22.5127V25.3732C31.2137 25.6257 31.0596 25.8536 30.8227 25.9513C30.7431 25.9839 30.6595 26 30.5764 26C30.4124 26 30.251 25.9375 30.1293 25.8199L26.7164 22.5184H19.1792C17.5424 22.5184 16.2154 21.2137 16.2154 19.6041V18.8428H25.2321C28.6551 18.8428 31.4399 16.1045 31.4399 12.7387Z" fill="#202020"/>
+                        </svg>   </a>                
+                </div>
+               
+        </div>
+		<div className="main">
+		<div class="services-container">
+            <div class="services">
+                    <div class="services__title"> 
+                        <svg width="20" height="16" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M12 0H8L0 16H4L12 0ZM20 0H16L8 16H12L20 0Z" fill="#FFCA00"/></svg>
+                    </div> 
+            </div>
+            
+            <div class="services__block">
+              
+			  
+			  
+			    <div class="block__element">
+                    <img class="element__img" src="http://localhost:3000/icons_Монтажная область 1.svg" alt="" />
+                    <div class="element__text">
+                        <div class="element__title"> Engineering Service </div>
+                        <p class="element__content"> Software Development / Software Testing / Solutions Architecture / Business Analysis / Operations Support  </p>
+                        <div class="element__arrow"><a href=""> <svg width="28" height="8" viewBox="0 0 28 8" fill="none" xmlns="http://www.w3.org/2000/svg"> 
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M24.2206 7.87969C24.063 8.0401 23.8006 8.0401 23.6375 7.87969C23.4798 7.72469 23.4798 7.46671 23.6375 7.31208L26.5945 4.40466L0.408252 4.40466C0.180792 4.4043 0 4.22654 0 4.00289C0 3.77924 0.180792 3.5957 0.408252 3.5957L26.5945 3.5957L23.6375 0.693704C23.4798 0.533285 23.4798 0.274952 23.6375 0.120314C23.8006 -0.0401049 24.0634 -0.0401049 24.2206 0.120314L27.8776 3.71601C28.0408 3.87101 28.0408 4.12899 27.8776 4.28362L24.2206 7.87969Z" fill="#202020"/>
+                            </svg></a>
+                            </div>
+                    </div>     
+                </div>        
+                <div class="block__element">
+                    <img class="element__img" src="http://localhost:3000/icons_Монтажная область 1 копия 2.png" alt="" />
+                    <div class="element__text">
+                        <div class="element__title"> Internet of Things  </div>
+                        <p class="element__content"> Intelligent Mobility / Predictive Maintenance / Smart Manufacturing </p>
+                        <div class="element__arrow"><a href=""><svg width="28" height="8" viewBox="0 0 28 8" fill="none" xmlns="http://www.w3.org/2000/svg"> 
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M24.2206 7.87969C24.063 8.0401 23.8006 8.0401 23.6375 7.87969C23.4798 7.72469 23.4798 7.46671 23.6375 7.31208L26.5945 4.40466L0.408252 4.40466C0.180792 4.4043 0 4.22654 0 4.00289C0 3.77924 0.180792 3.5957 0.408252 3.5957L26.5945 3.5957L23.6375 0.693704C23.4798 0.533285 23.4798 0.274952 23.6375 0.120314C23.8006 -0.0401049 24.0634 -0.0401049 24.2206 0.120314L27.8776 3.71601C28.0408 3.87101 28.0408 4.12899 27.8776 4.28362L24.2206 7.87969Z" fill="#202020"/>
+                            </svg></a>
+                            </div>
+                    </div>     
+                </div>        
+                <div class="block__element">
+                    <img class="element__img" src="http://localhost:3000/icons_Монтажная область 1 копия 3.png" alt="" />
+                    <div class="element__text">
+                        <div class="element__title"> Extended Reality XR  </div>
+                        <p class="element__content"> VR / MR / AR </p>
+                        <div class="element__arrow"><a href=""> <svg width="28" height="8" viewBox="0 0 28 8" fill="none" xmlns="http://www.w3.org/2000/svg"> 
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M24.2206 7.87969C24.063 8.0401 23.8006 8.0401 23.6375 7.87969C23.4798 7.72469 23.4798 7.46671 23.6375 7.31208L26.5945 4.40466L0.408252 4.40466C0.180792 4.4043 0 4.22654 0 4.00289C0 3.77924 0.180792 3.5957 0.408252 3.5957L26.5945 3.5957L23.6375 0.693704C23.4798 0.533285 23.4798 0.274952 23.6375 0.120314C23.8006 -0.0401049 24.0634 -0.0401049 24.2206 0.120314L27.8776 3.71601C28.0408 3.87101 28.0408 4.12899 27.8776 4.28362L24.2206 7.87969Z" fill="#202020"/>
+                            </svg></a>
+                            </div>
+                    </div>     
+                </div>        
+                <div class="block__element">
+                    <img class="element__img" src="http://localhost:3000/icons_Монтажная область 1 копия 4.png" alt="" />
+                    <div class="element__text">
+                        <div class="element__title"> AL & ML  </div>
+                        <p class="element__content"> Artificial Intelligence / Intelligent Automation / Advanced Analytics </p>
+                        <div class="element__arrow"><a href=""><svg width="28" height="8" viewBox="0 0 28 8" fill="none" xmlns="http://www.w3.org/2000/svg"> 
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M24.2206 7.87969C24.063 8.0401 23.8006 8.0401 23.6375 7.87969C23.4798 7.72469 23.4798 7.46671 23.6375 7.31208L26.5945 4.40466L0.408252 4.40466C0.180792 4.4043 0 4.22654 0 4.00289C0 3.77924 0.180792 3.5957 0.408252 3.5957L26.5945 3.5957L23.6375 0.693704C23.4798 0.533285 23.4798 0.274952 23.6375 0.120314C23.8006 -0.0401049 24.0634 -0.0401049 24.2206 0.120314L27.8776 3.71601C28.0408 3.87101 28.0408 4.12899 27.8776 4.28362L24.2206 7.87969Z" fill="#202020"/>
+                            </svg></a> 
+                            </div>
+                    </div>     
+                </div>        
+                <div class="block__element">
+                    <img class="element__img" src="http://localhost:3000/icons_Монтажная область 1 копия 5.png" alt="" />
+                    <div class="element__text">
+                        <div class="element__title"> Big data & Analytics  </div>
+                        <p class="element__content"> BI & Augmented Analytics / Enterprise Data Platforms / Data Strategy and Governance </p>
+                        <div class="element__arrow"><a href=""> <svg width="28" height="8" viewBox="0 0 28 8" fill="none" xmlns="http://www.w3.org/2000/svg"> 
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M24.2206 7.87969C24.063 8.0401 23.8006 8.0401 23.6375 7.87969C23.4798 7.72469 23.4798 7.46671 23.6375 7.31208L26.5945 4.40466L0.408252 4.40466C0.180792 4.4043 0 4.22654 0 4.00289C0 3.77924 0.180792 3.5957 0.408252 3.5957L26.5945 3.5957L23.6375 0.693704C23.4798 0.533285 23.4798 0.274952 23.6375 0.120314C23.8006 -0.0401049 24.0634 -0.0401049 24.2206 0.120314L27.8776 3.71601C28.0408 3.87101 28.0408 4.12899 27.8776 4.28362L24.2206 7.87969Z" fill="#202020"/>
+                            </svg></a>
+                            </div>
+                    </div>     
+                </div>        
+                <div class="block__element">
+                    <img class="element__img" src="http://localhost:3000/icons_Монтажная область 1 копия 6.png" alt="" />
+                    <div class="element__text">
+                        <div class="element__title"> Blockchain  </div>
+                        <p class="element__content"> Создаем прибыльные интернет-магазины, маркетплейсы и агрегаторы </p>
+                        <div class="element__arrow"><a href=""> <svg width="28" height="8" viewBox="0 0 28 8" fill="none" xmlns="http://www.w3.org/2000/svg"> 
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M24.2206 7.87969C24.063 8.0401 23.8006 8.0401 23.6375 7.87969C23.4798 7.72469 23.4798 7.46671 23.6375 7.31208L26.5945 4.40466L0.408252 4.40466C0.180792 4.4043 0 4.22654 0 4.00289C0 3.77924 0.180792 3.5957 0.408252 3.5957L26.5945 3.5957L23.6375 0.693704C23.4798 0.533285 23.4798 0.274952 23.6375 0.120314C23.8006 -0.0401049 24.0634 -0.0401049 24.2206 0.120314L27.8776 3.71601C28.0408 3.87101 28.0408 4.12899 27.8776 4.28362L24.2206 7.87969Z" fill="#202020"/>
+                            </svg></a>
+                            </div>
+                    </div>     
+                </div>        
+                <div class="block__element">
+                    <img class="element__img" src="http://localhost:3000/icons_Монтажная область 1 копия.png" alt="" />
+                    <div class="element__text">
+                        <div class="element__title"> Game Art Production </div>
+                        <p class="element__content"> Concept art, UI, illustration / 3D models and environments / VXT and animations </p>
+                        <div class="element__arrow"><a href=""><svg width="28" height="8" viewBox="0 0 28 8" fill="none" xmlns="http://www.w3.org/2000/svg"> 
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M24.2206 7.87969C24.063 8.0401 23.8006 8.0401 23.6375 7.87969C23.4798 7.72469 23.4798 7.46671 23.6375 7.31208L26.5945 4.40466L0.408252 4.40466C0.180792 4.4043 0 4.22654 0 4.00289C0 3.77924 0.180792 3.5957 0.408252 3.5957L26.5945 3.5957L23.6375 0.693704C23.4798 0.533285 23.4798 0.274952 23.6375 0.120314C23.8006 -0.0401049 24.0634 -0.0401049 24.2206 0.120314L27.8776 3.71601C28.0408 3.87101 28.0408 4.12899 27.8776 4.28362L24.2206 7.87969Z" fill="#202020"/>
+                            </svg></a>
+                            </div>
+                    </div>     
+                </div>        
+            </div>
+        </div>
+		</div>
+	
+	</div>
+
+ 
+export default App;
